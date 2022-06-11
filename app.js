52828b79ddecdff4f49ae8f0ff45af71f2fe4d7c
agunsatrio <76941841+agunsatrio@users.noreply.github.com>
2022-06-11 23:14:31 +0700

    instalasi express.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use("assets", express.static("assets"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen("3000", () => {
+  console.log("app runing at port 3000");
+});
