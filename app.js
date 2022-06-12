446ab5092b485fbe5a5f33e5b9e127f793dac4c4
agunsatrio <76941841+agunsatrio@users.noreply.github.com>
2022-06-12 10:00:42 +0700

    menambahkan router dan controller

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const { path } = require("path");
+// const methodOverride = require("method-override");
+// const { path } = require("path");
+const homeRouter = require("./routers/homeRouter");
 
 const app = express();
 
@@ -8,9 +10,11 @@ app.set("views", "views");
 
 app.use("/assets", express.static("assets"));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json);
+// app.use(methodOverride("_method"));
+
+app.use("/", homeRouter);
 
 app.listen("3000", () => {
   console.log("app runing at port 3000");
