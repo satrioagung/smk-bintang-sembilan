268ae1d9bf58bd92efec897cbe953e8274f0487a
agunsatrio <76941841+agunsatrio@users.noreply.github.com>
2022-06-16 10:13:54 +0700

    menambahkan loginModel

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,6 +1,7 @@
 const con = require("../config/database");
 
 let mysql = require("mysql");
+const loginModel = require("../models/loginModel");
 let pool = mysql.createPool(con);
 
 pool.on("error", (err) => {
@@ -10,36 +11,32 @@ pool.on("error", (err) => {
 module.exports = {
   login(req, res) {
     res.render("login/login", {
-      url: "http://localhost:3000/",
       colorFlash: req.flash("color"),
       statusFlash: req.flash("status"),
       pesanFlash: req.flash("message"),
     });
   },
+
   loginAuth(req, res) {
     let email = req.body.email;
     let password = req.body.pass;
     if (email && password) {
       pool.getConnection(function (err, connection) {
         if (err) throw err;
-        connection.query(
-          `SELECT * FROM table_user WHERE user_email = ? AND user_password = SHA2(?,512)`,
-          [email, password],
-          function (error, results) {
-            if (error) throw error;
-            if (results.length > 0) {
-              req.session.loggedin = true;
-              req.session.userid = results[0].user_id;
-              req.session.username = results[0].user_name;
-              res.redirect("/dashboard");
-            } else {
-              req.flash("color", "danger");
-              req.flash("status", "Oops..");
-              req.flash("message", "Akun tidak ditemukan");
-              res.redirect("/login");
-            }
+        loginModel.getUser(connection, req.body, (err, results) => {
+          if (err) throw error;
+          if (results.length > 0) {
+            req.session.loggedin = true;
+            req.session.userid = results[0].user_id;
+            req.session.username = results[0].user_name;
+            res.redirect("/dashboard");
+          } else {
+            req.flash("color", "danger");
+            req.flash("status", "Oops..");
+            req.flash("message", "Akun tidak ditemukan");
+            res.redirect("/login");
           }
-        );
+        });
         connection.release();
       });
     } else {
@@ -47,6 +44,7 @@ module.exports = {
       res.end();
     }
   },
+
   logout(req, res) {
     req.session.destroy((err) => {
       if (err) {
@@ -56,11 +54,11 @@ module.exports = {
       res.redirect("/login");
     });
   },
+
   formRegister(req, res) {
-    res.render("login/register", {
-      url: "http://localhost:3000/",
-    });
+    res.render("login/register");
   },
+
   saveRegister(req, res) {
     let username = req.body.username;
     let email = req.body.email;
@@ -68,17 +66,13 @@ module.exports = {
     if (username && email && password) {
       pool.getConnection(function (err, connection) {
         if (err) throw err;
-        connection.query(
-          `INSERT INTO table_user (user_name,user_email,user_password) VALUES (?,?,SHA2(?,512));`,
-          [username, email, password],
-          function (error, results) {
-            if (error) throw error;
-            req.flash("color", "success");
-            req.flash("status", "Yes..");
-            req.flash("message", "Registrasi berhasil");
-            res.redirect("/login");
-          }
-        );
+        loginModel.createUser(connection, req.body, (err, results) => {
+          if (err) throw error;
+          req.flash("color", "success");
+          req.flash("status", "Yes..");
+          req.flash("message", "Registrasi berhasil");
+          res.redirect("/login");
+        });
         connection.release();
       });
     } else {
@@ -86,4 +80,5 @@ module.exports = {
       res.end();
     }
   },
+  
 };
---
+++ models/loginModel.js
@@ -0,0 +1,26 @@
+const con = require("../config/database");
+
+let mysql = require("mysql");
+let pool = mysql.createPool(con);
+
+pool.on("error", (err) => {
+  console.error(err);
+});
+
+module.exports = {
+  getUser: (connect, data, cb) => {
+    connect.query(
+      `SELECT * FROM table_user WHERE user_email = ? AND user_password = SHA2(?,512)`,
+      [data.email, data.pass],
+      cb
+    );
+  },
+
+  createUser: (connect, data, cb) => {
+    connect.query(
+      `INSERT INTO table_user (user_name,user_email,user_password) VALUES (?,?,SHA2(?,512));`,
+      [data.username, data.email, data.pass],
+      cb
+    );
+  },
+};
