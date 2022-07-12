31f8af8afdb56e613e265085f85a1b58ce21f5a0
agunsatrio <76941841+agunsatrio@users.noreply.github.com>
2022-07-12 10:58:31 +0700

    restore database

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   multipleStatements: true,
   host: "db4free.net",
-  user: "bintang9",
-  password: "6bc99547",
-  database: "smkbintang9",
+  user: "bintang9bm",
+  password: "wCbZ3vG!B7wMmDs",
+  database: "smkbintang9bm",
 };
